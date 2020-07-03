03b4842a1cc5594155af23e154cbc42d3ea76d0b
Benny Garcia <bennygarcia06@gmail.com>
2020-07-03 11:08:32 -0400

    Added refresh interval
    Added refresh interval to page.
    Bug fixes. Trying to add tags to the arrays so they print to page.

--- js/script.js
+++ js/script.js
@@ -14,16 +14,18 @@ const quotes = [
   {
     quote: "I came, I saw, I conquered.",
     source: "Julius Ceasar",
-    citation: "Letter to Raoman Senate",
+    citation: "Letter to Roman Senate",
     year: "47 BC"
   },
   {
     quote: "The way to get started is to quit talking and begin doing.",
-    source: "Walt Disney"
+    source: "Walt Disney",
+    tags: "motivation"
   },
   {
     quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
-    source: "James Cameron"
+    source: "James Cameron",
+    tags: ["goals", "motivation"]
   },
   {
     quote: "The future belongs to those who believe in the beauty of their dreams.",
@@ -31,7 +33,8 @@ const quotes = [
   },
   {
     quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
-    source: "Benjamin Franklin"
+    source: "Benjamin Franklin",
+    tags: "education"
   }
 ];
 
@@ -62,22 +65,20 @@ function printQuote() {
   // function
   const callRandomQuote = getRandomQuote();
 
-  console.log(getRandomQuote());
-  
   // 2. Create a variable that initiates your HTML string with 
   // the first two <p></p> elements, their classNames, 
   // and the quote and source properties, but leave off 
   // the second closing `</p>` tag for now
-let htmlString = `<p class="quotes">${getRandomQuote.quotes}</p>`
-                 `<p class="source">${getRandomQuote.source}`
+  let htmlString = `<p class="quote">${callRandomQuote.quote}</p>
+                    <p class="source">${callRandomQuote.source}`;
   
   // 3. Use an if statement to check if the citation property 
   // exists, and if it does, concatenate a <span></span> 
   // element, appropriate className, and citation property 
   // to the HTML string
 
-  if (quotes.citation) {
-    htmlString += `<span class="citation">${quotes.citation}</span>`;
+  if (callRandomQuote.citation) {
+    htmlString += `<span class="citation">${callRandomQuote.citation}</span>`;
   } 
 
   // 4. Use an if statement to check of the year property exists, 
@@ -85,20 +86,26 @@ let htmlString = `<p class="quotes">${getRandomQuote.quotes}</p>`
   // appropriate className, and year property to the HTML 
   //string
 
-  if (quotes.year) {
-    htmlString += `<span class="year">${quotes.year}</span>`;
+  if (callRandomQuote.year) {
+    htmlString += `<span class="year">${callRandomQuote.year}</span>`;
+  } 
+  
+  if (callRandomQuote.tags) {
+    htmlString += `<span class="tags">${callRandomQuote.tags.join(', ')}</span>`;
   } else {
     htmlString += `</p>`;
   }
-
-  document.getElementById('quote-box').innerHTML = htmlString;
-
   // 5. After the two if statements, concatenate the closing </p> 
   // tag to the HTML string
 
   // 6. set the innerHTML of the quote-box div to equal the 
   // complete HTML string
+  document.getElementById('quote-box').innerHTML = htmlString;
 }
+
+
+
+setInterval(printQuote, 5000);
 
 
 /***
