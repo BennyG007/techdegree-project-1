ff205fab09df7b4f2bf92a0b864393dbfd26f568
Benny Garcia <bennygarcia06@gmail.com>
2020-07-03 08:10:59 -0400

    printQuote function
    the pringQuote function section has been updated. Step 1 - variable that calls a random quote.

--- js/script.js
+++ js/script.js
@@ -39,12 +39,8 @@ const quotes = [
  * `getRandomQuote` function
  * This function grabs a random quote from the Quotes array above.
 ***/
-function getRandomQuote() {
-  const randomNum = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomNum];
-  return randomQuote;
-    
-  // 1. Create a variable that generates a random number
+
+// 1. Create a variable that generates a random number
   // between zero and the last index in the `quotes` array
 
   // 2. Use the random number variable and bracket notation 
@@ -52,17 +48,57 @@ function getRandomQuote() {
   // store it in a variable
 
   // 3. Return the variable storing the random quote object
+function getRandomQuote() {
+  const randomNum = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomNum];
+  return randomQuote;  
 }
-
-
-
-
-
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  // 1. Create a variable that calls the getRandomQuote() 
+  // function
+  const callRandomQuote = getRandomQuote();
 
+  console.log(getRandomQuote());
+  
+  // 2. Create a variable that initiates your HTML string with 
+  // the first two <p></p> elements, their classNames, 
+  // and the quote and source properties, but leave off 
+  // the second closing `</p>` tag for now
+let htmlString = `<p class="quotes">${getRandomQuote.quotes}</p>`
+                 `<p class="source">${getRandomQuote.source}`
+  
+  // 3. Use an if statement to check if the citation property 
+  // exists, and if it does, concatenate a <span></span> 
+  // element, appropriate className, and citation property 
+  // to the HTML string
+
+  if (quotes.citation) {
+    htmlString += `<span class="citation">${quotes.citation}</span>`;
+  } 
+
+  // 4. Use an if statement to check of the year property exists, 
+  // and if it does, concatenate a <span></span> element, 
+  // appropriate className, and year property to the HTML 
+  //string
+
+  if (quotes.year) {
+    htmlString += `<span class="year">${quotes.year}</span>`;
+  } else {
+    htmlString += `</p>`;
+  }
+
+  document.getElementById('quote-box').innerHTML = htmlString;
+
+  // 5. After the two if statements, concatenate the closing </p> 
+  // tag to the HTML string
+
+  // 6. set the innerHTML of the quote-box div to equal the 
+  // complete HTML string
+}
 
 
 /***
