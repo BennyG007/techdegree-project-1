54d149b7fc6b9a2bba14480ca125bd884c7105e0
Benny Garcia <bennygarcia06@gmail.com>
2020-07-03 15:17:16 -0400

    Fixed Tags
    Fixed the issue with the tags. Tags now properly display on webpage as well.
    
    Updated some comments.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# techdegree-project-1
+TechDegree Project 1
  Random Quote Generator
+
+ The purpose of this project is to select a random quote from an array of objects and displays that quote on the web page. Everytime the user clicks "show another quote" you will get a random quote. 
+
+ If nothing is selected, the page will auto refresh every 5 seconds with a new quote. 
+
+ The background color of the page also changes everytime you click the button or no action is taken for 5 seconds. 
--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * Below is an array of quotes. Some quotes include who said it, the year it was said and certain attributes. 
 ***/
 const quotes = [
   {
@@ -20,7 +20,7 @@ const quotes = [
   {
     quote: "The way to get started is to quit talking and begin doing.",
     source: "Walt Disney",
-    tags: "motivation"
+    tags: ["motivation"]
   },
   {
     quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
@@ -34,23 +34,16 @@ const quotes = [
   {
     quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
     source: "Benjamin Franklin",
-    tags: "education"
+    tags: ["education"]
   }
 ];
 
 /***
- * `getRandomQuote` function
- * This function grabs a random quote from the Quotes array above.
-***/
+ * The purpose of the random quote function is to grab a quote from the array above at random. It will ultimately be used to display that random quote on the web page. 
+ ***/
 
-// 1. Create a variable that generates a random number
-  // between zero and the last index in the `quotes` array
+// The getRandomQuote() function generates a random number. This is used to grab a random ojbect from the array of quotes above.  
 
-  // 2. Use the random number variable and bracket notation 
-  // to grab a random object from the `quotes` array, and 
-  // store it in a variable
-
-  // 3. Return the variable storing the random quote object
 function getRandomQuote() {
   const randomNum = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomNum];
@@ -58,60 +51,43 @@ function getRandomQuote() {
 }
 
 /***
- * `printQuote` function
+ * `printQuote` function - this function will print out a random quote to the webpage. 
 ***/
 function printQuote() {
-  // 1. Create a variable that calls the getRandomQuote() 
-  // function
   const callRandomQuote = getRandomQuote();
 
-  // 2. Create a variable that initiates your HTML string with 
-  // the first two <p></p> elements, their classNames, 
-  // and the quote and source properties, but leave off 
-  // the second closing `</p>` tag for now
   let htmlString = `<p class="quote">${callRandomQuote.quote}</p>
                     <p class="source">${callRandomQuote.source}`;
   
-  // 3. Use an if statement to check if the citation property 
-  // exists, and if it does, concatenate a <span></span> 
-  // element, appropriate className, and citation property 
-  // to the HTML string
+  // The following if statements will check if the objects contain citations, years or tags. If the statement is true, it will print to the page, if false, then it will close the strings above. 
 
   if (callRandomQuote.citation) {
     htmlString += `<span class="citation">${callRandomQuote.citation}</span>`;
   } 
 
-  // 4. Use an if statement to check of the year property exists, 
-  // and if it does, concatenate a <span></span> element, 
-  // appropriate className, and year property to the HTML 
-  //string
-
   if (callRandomQuote.year) {
     htmlString += `<span class="year">${callRandomQuote.year}</span>`;
+  } 
+  
+  if (callRandomQuote.tags) {
+    htmlString += `<span class="tags">, ${callRandomQuote.tags.join(', ')}</span>`;
   } else {
     htmlString += `</p>`;
   }
-  
 
-  // // if (callRandomQuote.tags) {
-  // //   htmlString += `<span class="tags">${callRandomQuote.tags.join(', ')}</span>`;
-  // } else {
-  //   htmlString += `</p>`;
-  // // }
-  // 5. After the two if statements, concatenate the closing </p> 
-  // tag to the HTML string
-
-  // 6. set the innerHTML of the quote-box div to equal the 
-  // complete HTML string
   document.getElementById('quote-box').innerHTML = htmlString;
 }
-console.log(typeof callRandomQuote);
 
+function randColor () {
+  return Math.floor(Math.random() * 256);
+}
 
+function randomBackgroun () {
+  
+}
 
-
+// This simply refreshes the page automatically every 5 seconds if no action is taken. 
 setInterval(printQuote, 5000);
-
 
 /***
  * click event listener for the print quote button
